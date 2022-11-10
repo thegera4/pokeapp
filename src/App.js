eddbd59600bfdc788852d3e1692fce97427fd104
thegera4 <84020433+thegera4@users.noreply.github.com>
2022-11-09 20:27:40 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import PkmnCard from './components/PkmnCard';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(0);
+  const [pokemons, setPokemons] = useState([]);
+
+  function handleOffset(e) {
+    setOffset(e.target.value);
+  }
+
+  function handleLimit(e) {
+    setLimit(e.target.value);
+  }
+
+  function handleClick(){
+    axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+      .then(response => {
+        setData(response.data.results);
+      })
+  }
+
+  useEffect(() => {
+    if (data) {
+      const promises = data?.map(pokemon => axios.get(pokemon.url));
+      Promise.all(promises)
+      .then(responses => {
+        const creatures = responses.map(response => {
+          return {
+            name: response.data.name,
+            sprite: response.data.sprites.front_default
+          }
+        })
+        setPokemons(creatures);
+      })
+    }
+  }, [data])
+
+  function onDelete (index){
+    const newPokemons = [...pokemons];
+    newPokemons.splice(index, 1);
+    setPokemons(newPokemons);
+  }
+
+  useEffect(() => {
+    //rerender pokemons when they one is deleted
+  }, [pokemons])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <div>
+        <label htmlFor="offset">Offset: </label>
+        <input type="number" name="offset" id="offset" onChange={handleOffset} />
+      </div>
+      <div>
+        <label htmlFor="limit">Limit: </label>
+        <input type="number" name="limit" id="limit" onChange={handleLimit}/>
+      </div>
+      </div>
+      <div>
+        <button onClick={()=>handleClick()}>
+          Get Pokemons
+        </button>
+      </div>
+      <PkmnCard pokemons={pokemons} onDelete={onDelete}/>
     </div>
   );
 }
---
+++ src/components/PkmnCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../App.css';
+
+function PkmnCard({pokemons, onDelete}) {
+  return (
+      <div className='container'>
+        {pokemons.map((pokemon, index) => (
+          <div key={index} className='card'>
+            <button className='delete' onClick={()=>onDelete(index)}>
+              X
+            </button>
+            <img src={pokemon.sprite} alt={pokemon.name} />
+            <p>{pokemon.name}</p>
+          </div>
+        ))}
+      </div>
+  )
+}
+
+export default PkmnCard
